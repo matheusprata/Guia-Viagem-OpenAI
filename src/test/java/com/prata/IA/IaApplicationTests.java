eb3be5b5fd5c74dbf195e582c5bc7a4774a4b598
matheusprata <matheusprata.ti@gmail.com>
2024-04-24 22:29:18 -0300

    iniciando projeto de comunicação com a OpenAi

---
+++ src/test/java/com/prata/IA/IaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prata.IA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
